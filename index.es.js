499ce60a7f4c592c30817045c0d848e690c0dfb9
webbaipiao <2244713679@qq.com>
2023-11-06 23:47:00 +0800

    feat: update ESModule export

--- index.es.js
+++ index.es.js
@@ -1,7 +1,7 @@
-export { default as formatStringToBigHumpString } from './format-string-to-big-hump-string';
-export { default as formatStringToHumpString } from './format-string-to-hump-string';
-export { default as formatStringToUppercase } from './format-string-to-uppercase';
-export { default as getBetweenRandomNumber } from './get-between-random-number';
-export { default as jsonParse } from './json-parse';
-export { default as randomSleep } from './random-sleep';
-export { default as sleep } from './sleep';
+export { default as formatStringToBigHumpString } from './format-string-to-big-hump-string/index.js';
+export { default as formatStringToHumpString } from './format-string-to-hump-string/index.js';
+export { default as formatStringToUppercase } from './format-string-to-uppercase/index.js';
+export { default as getBetweenRandomNumber } from './get-between-random-number/index.js';
+export { default as jsonParse } from './json-parse/index.js';
+export { default as randomSleep } from './random-sleep/index.js';
+export { default as sleep } from './sleep/index.js';
